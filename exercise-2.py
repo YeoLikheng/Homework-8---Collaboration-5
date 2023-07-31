149d50a5c58de202391ceafcefdd1c810cd3899c
YeoLikheng <2023342yeo@aupp.edu.kh>
2023-07-31 18:38:48 +0700

    Homework

--- exercise-2.py
+++ exercise-2.py
@@ -1,2 +1,7 @@
 def index_power(numnbers, n):
-    ...
+    for i in numnbers:
+        if numnbers.index(i) == n:
+            result = i**n
+            return result
+
+print(index_power([1,2,3,4],3))

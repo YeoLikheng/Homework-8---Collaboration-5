96b2373080d36e40b2be4c20d6ffb7d292134956
nithielim <2023136lim@aupp.edu.kh>
2023-08-02 16:03:18 +0700

    Update exercise-5.py

--- exercise-5.py
+++ exercise-5.py
@@ -1,33 +1,20 @@
 def reverse_ascending(numbers):
-    if not numbers:
+    if not numbers:  # if the list is empty
         return numbers
-    result = []
-    current_subsequence = [numbers[0]]
+    # Initialize variables
+    res, temp = [], [numbers[0]]
+    # Iterate through the numbers
     for i in range(1, len(numbers)):
-        if numbers[i] > numbers[i-1]:
-            current_subsequence.append(numbers[i])
-        else:
-            current_subsequence.reverse()
-            result.extend(current_subsequence)
-            current_subsequence = [numbers[i]]
-    current_subsequence.reverse()
-    result.extend(current_subsequence)
-    return result
+        if numbers[i] > numbers[i-1]:  # If the current number is greater than the previous one, it's part of an ascending subsequence
+            temp.append(numbers[i])
+        else:  # If it's not part of the same ascending subsequence, append the reversed subsequence to the result
+            res += temp[::-1]
+            temp = [numbers[i]]  # Start a new subsequence
+    res += temp[::-1]  # Append the last subsequence
+    
 
-# Run the tests
-test_cases = [
-    [1, 2, 3, 4, 5],
-    [5, 7, 10, 4, 2, 7, 8, 1, 3],
-    [5, 4, 3, 2, 1],
-    []
-]
-
-# Output for first test case
-result = reverse_ascending(test_cases[1])
-print('\n'.join(map(str, result)))
-
-print()
-
-# Output for second test case
-for i in range(1, 15):
-    print(i)
+    return res
+assert list(reverse_ascending([1, 2, 3, 4, 5])) == [5, 4, 3, 2, 1]
+assert list(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3])) == [10, 7, 5, 4, 8, 7, 2, 3, 1]
+assert list(reverse_ascending([5, 4, 3, 2, 1])) == [5, 4, 3, 2, 1]
+assert list(reverse_ascending([])) == []

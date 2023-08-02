8a7b0593ec1c4f9d0c1abd492479de19669a5934
Mengly198 <2023194lay@aupp.edu.kh>
2023-08-02 12:03:13 +0700

    exercise4 done

--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,7 @@
 def chunking_by(numbers, chunck):
     ...
+    chunked_list = []
+    for i in range(0, len(numbers), chunck):
+        chunked_list.append(numbers[i:i+chunck])
+
+    return chunked_list
